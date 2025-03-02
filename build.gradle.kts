2656c66227c8db16c98b622df26bd769f315dc94
Dyrk2020 <Dyrk2020@users.noreply.github.com>
2025-03-02 10:15:00 +0800

    Set up Gradle scaffolding and plugin registration

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.0.20"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
+    kotlin("plugin.serialization") version "2.0.20"
+}
+
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+
+        defaultRepositories()
+    }
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellijPlatform {
+    pluginConfiguration {
+        name = "Code Time"
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
+val ktorVersion: String by project
+
+intellijPlatform {
+    // ...
+
+    pluginVerification {
+        // ...
+
+        ides {
+            ide(IntelliJPlatformType.IntellijIdeaUltimate, "2024.3")
+        }
+    }
+}
+dependencies {
+    intellijPlatform {
+        intellijIdeaUltimate("2024.3")
+        bundledPlugin("com.intellij.java")
+
+        pluginVerifier()
+        zipSigner()
+        instrumentationTools()
+
+        testFramework(TestFrameworkType.Platform)
+    }
+    implementation("org.slf4j:slf4j-nop:2.0.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+
+}
+
